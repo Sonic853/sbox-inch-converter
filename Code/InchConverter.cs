3d8229fd9e0b28b8e21028b2796d4d82501f9bf7
Sonic853 <Sonic853@qq.com>
2024-10-30 03:17:17 +0800

    Replacement correction

--- Code/InchConverter.cs
+++ Code/InchConverter.cs
@@ -3,25 +3,31 @@ namespace Sonic853.InchConverterLib
 {
     public static class InchConverter
     {
-        public static float MToIn( float m ) => m * 39.3701f;
-        public static float MToCm( float m ) => m * 100f;
-        public static float CmToIn( float cm ) => cm * 0.393701f;
-        public static float CmToM( float cm ) => cm * 0.01f;
-        public static float InToM( float inch ) => inch * 0.0254f;
-        public static float InToCm( float inch ) => inch * 2.54f;
-        public static Vector3 MToIn( Vector3 m ) => m * 39.3701f;
-        public static Vector3 MToCm( Vector3 m ) => m * 100f;
-        public static Vector3 CmToIn( Vector3 cm ) => cm * 0.393701f;
-        public static Vector3 CmToM( Vector3 cm ) => cm * 0.01f;
-        public static Vector3 InToM( Vector3 inch ) => inch * 0.0254f;
-        public static Vector3 InToCm( Vector3 inch ) => inch * 2.54f;
-        public static Vector2 MToIn( Vector2 m ) => m * 39.3701f;
-        public static Vector2 MToCm( Vector2 m ) => m * 100f;
-        public static Vector2 CmToIn( Vector2 cm ) => cm * 0.393701f;
-        public static Vector2 CmToM( Vector2 cm ) => cm * 0.01f;
-        public static Vector2 InToM( Vector2 inch ) => inch * 0.0254f;
-        public static Vector2 InToCm( Vector2 inch ) => inch * 2.54f;
-        public static Vector3 ToM( this Vector3 inch ) => inch * 0.0254f;
-        public static Vector3 ToIn( this Vector3 m ) => m * 39.3701f;
+        const float mtocm = 100f;
+        const float cmtom = 0.01f;
+        const float intom = 0.0254f;
+        const float intocm = 2.54f;
+        const float mtoin = 100 / intocm;
+        const float cmtoin = 1 / intocm;
+        public static float MToIn( float m ) => m * mtoin;
+        public static float MToCm( float m ) => m * mtocm;
+        public static float CmToIn( float cm ) => cm * cmtoin;
+        public static float CmToM( float cm ) => cm * cmtom;
+        public static float InToM( float inch ) => inch * intom;
+        public static float InToCm( float inch ) => inch * intocm;
+        public static Vector3 MToIn( Vector3 m ) => m * mtoin;
+        public static Vector3 MToCm( Vector3 m ) => m * mtocm;
+        public static Vector3 CmToIn( Vector3 cm ) => cm * cmtoin;
+        public static Vector3 CmToM( Vector3 cm ) => cm * cmtom;
+        public static Vector3 InToM( Vector3 inch ) => inch * intom;
+        public static Vector3 InToCm( Vector3 inch ) => inch * intocm;
+        public static Vector2 MToIn( Vector2 m ) => m * mtoin;
+        public static Vector2 MToCm( Vector2 m ) => m * mtocm;
+        public static Vector2 CmToIn( Vector2 cm ) => cm * cmtoin;
+        public static Vector2 CmToM( Vector2 cm ) => cm * cmtom;
+        public static Vector2 InToM( Vector2 inch ) => inch * intom;
+        public static Vector2 InToCm( Vector2 inch ) => inch * intocm;
+        public static Vector3 ToM( this Vector3 inch ) => inch * intom;
+        public static Vector3 ToIn( this Vector3 m ) => m * mtoin;
     }
 }
